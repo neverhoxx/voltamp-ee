31aff9b201e9ac9d9e846fcce49f15007fc3b6ef
neverhoxx <yehor.kiiakh@gmail.com>
2025-08-22 08:15:48 +0300

    link errors 500

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,51 @@ const nextConfig: NextConfig = {
         destination: '/et/integreeritav-paikesekatus/taiskatuse-susteem',
         permanent: true,
       },
+      {
+        source: '/p%C3%A4ikesepaneelide-seadmed',
+        destination: '/et/tooted/seadmed',
+        permanent: true,
+      },
+      {
+        source: '/katuset%C3%B6%C3%B6d',
+        destination: '/et/katuse-paigaldus',
+        permanent: true,
+      },
+      {
+        source: '/katuse-paigaldus',
+        destination: '/et/katuse-paigaldus',
+        permanent: true,
+      },
+      {
+        source: '/pildigalerii/seadmed',
+        destination: '/et/tooted/seadmed',
+        permanent: true,
+      },
+      {
+        source: '/pildigalerii/standardlahendus',
+        destination: '/et',
+        permanent: true,
+      },
+      {
+        source: '/k%C3%B5ik-p%C3%A4ikesepaneelide-seadmed',
+        destination: '/et/tooted/seadmed',
+        permanent: true,
+      },
+      {
+        source: '/meist',
+        destination: '/et/meist',
+        permanent: true,
+      },
+      {
+        source: '/pildigalerii/autovarjualune',
+        destination: '/et',
+        permanent: true,
+      },
+      {
+        source: '/pildigalerii/klassikaline-katus',
+        destination: '/et',
+        permanent: true,
+      },
     ];
   },
 
